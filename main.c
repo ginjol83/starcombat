d3a99a4ebbe79bf3348091e2b0752305d34c401e
agimenez122 <andresgimpe@gmail.com>
2021-09-16 23:25:42 +0200

    Add lives drop functionality and clean code

--- main.c
+++ main.c
@@ -7,15 +7,13 @@
 
 //includes
 #include <gb/gb.h>
-#include <stdio.h>
-#include <ctype.h>
-#include <rand.h>
 #include <gb/console.h>
 #include <gb/drawing.h>
+#include <stdio.h>
 
 //imports
 #include "../projects/StarCombat/Characters/flight.c"
-#include "../projects/StarCombat/Characters/enemigo.c"
+#include "../projects/StarCombat/Characters/enemy.c"
 
 #include "../projects/StarCombat/Libreries/collisions.h"
 #include "../projects/StarCombat/Libreries/backgrounds.h"
@@ -34,8 +32,10 @@ void updateSwitches();
 // array will hold the player's position as X ([0]) and Y ([1])
 int player[2];
 int enemy[2];
+int lives;
 const int initialX = 80;
-const int initialY = 72;
+const int initialY = 120;
+
 
 void main() {
 
@@ -51,35 +51,33 @@ void main() {
 }
 
 void init() {
-	
 	DISPLAY_ON;						// Turn on the display
 	plot_intro_image(jujube_tile_data,jujube_map_data);
 	plot_background_image(startScreen_tile_data,startScreen_map_data);
-	
 }
 
 void firstLevel(){
-	plot_intro_image(firstLevelBackground_tile_data,firstLevelBackground_map_data);
-
-	// Load the the 'sprites' tiles into sprite memory
-	set_sprite_data(0, 0, flight);
-	
-	// Set the first movable sprite (0) to be the first tile in the sprite memory (0)
-	set_sprite_tile(0,0);
+	plot_background_image(firstLevelBackground_tile_data,firstLevelBackground_map_data);
 
 	player[0] = initialX;
 	player[1] = initialY;
-
-
-	// enemy
+	enemy[0] = 100;
+	enemy[1] = 92; 
+	// Load the the 'sprites' tiles into sprite memory
+	set_sprite_data(0, 0, flight);
 	set_sprite_data(1, 1, enemyTile);
+
+	// Set the first movable sprite (0) to be the first tile in the sprite memory (0)
+	set_sprite_tile(0,0);
 	set_sprite_tile(1,1);
 	set_sprite_tile(2,1);
 	set_sprite_tile(3,1);
 	set_sprite_tile(4,1);
 
-	enemy[0] = 100;
-	enemy[1] = 92; 
+	//lives
+	set_sprite_tile(5,0);
+	set_sprite_tile(6,0);
+	set_sprite_tile(7,0);
 }
 
 void updateSwitches() {
@@ -95,6 +93,7 @@ void checkInput() {
 
 	// START
     if (joypad() & J_START) {
+		lives=3;
 		firstLevel();
     }
 
@@ -141,9 +140,27 @@ void checkInput() {
 	move_sprite(3, enemy[0]-40, enemy[1]-40);
 	move_sprite(4, enemy[0]-50, enemy[1]-20);
 
+	//lives
+	if(lives>2){move_sprite(5, 130, 145);}
+	if(lives>1){move_sprite(6, 140, 145);}
+	if(lives>0){move_sprite(7, 150, 145);}
 
-	if(collisionCheck(player[0], player[1], 8, 8, enemy[0], enemy[1], 8, 8) == 1) {
+	if(collisionCheck(player[0], player[1], 8, 8, enemy[0]-10, enemy[1]-30, 8, 8) == 1) {
+
+		lives--;
+		//printf("live %d",lives);
+		player[0] = initialX;
+		player[1] = initialY;
+		move_sprite(0, player[0], player[1]);
+
+		if(lives<3){hide_sprite(5);}
+		if(lives<2){hide_sprite(6);}
+		if(lives<1){hide_sprite(7);}
 		
+		if(lives==0){
+			printf("game over");
+		}
+		delay(2000);
 	} 
 
 }
